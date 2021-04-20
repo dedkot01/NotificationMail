9e57378d5e35ab1a5453a06c39b15ea151ae29ea
dedkot <dedkot01@gmail.com>
2021-04-20 18:32:59 +0300

    set private fields: conf and email; added comments

--- src/main/scala/org/dedkot/EmailClient.scala
+++ src/main/scala/org/dedkot/EmailClient.scala
@@ -3,11 +3,15 @@ package org.dedkot
 import com.typesafe.config.ConfigFactory
 import org.apache.commons.mail.{DefaultAuthenticator, SimpleEmail}
 
+/** Singleton object. Implements sending email messages.
+ *
+ * Configurable in application.conf.
+ */
 object EmailClient {
 
-  val conf = ConfigFactory.load()
+  private val conf = ConfigFactory.load()
 
-  val email = new SimpleEmail()
+  private val email = new SimpleEmail()
 
   email.setHostName(conf.getString("email.hostName"))
   email.setSmtpPort(conf.getInt("email.smtpPort"))
@@ -17,6 +21,13 @@ object EmailClient {
     conf.getString("email.authenticator.password")))
   email.setFrom(conf.getString("email.from"))
 
+  /** Sends the simple email.
+   *
+   *  @param subject the email subject.
+   *  @param text define the content of the mail.
+   *  @param destinationEmail add a recipient TO to the email.
+   *  @return the message id of the underlying MimeMessage.
+   */
   def sendSimpleMsg(subject: String, text: String, destinationEmail: String): String = {
     email.setSubject(subject)
       .setMsg(text)
