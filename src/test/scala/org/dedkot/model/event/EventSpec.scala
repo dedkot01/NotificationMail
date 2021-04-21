cca2dba92d28f55e4d5b0bd6a8366af0e3aaa329
dedkot <dedkot01@gmail.com>
2021-04-21 13:56:51 +0300

    added test for Event model

---
+++ src/test/scala/org/dedkot/model/event/EventSpec.scala
@@ -0,0 +1,19 @@
+package org.dedkot.model.event
+
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
+
+import java.time.Instant
+
+class EventSpec extends AnyFlatSpec with should.Matchers {
+
+  "Event" should "return correct String" in {
+    val event = Event("0001-00001", Stage.STAGE1, Status.SUCCESS, Instant.ofEpochSecond(1618999810))
+    event.toString should be(
+      """Пользователь: 00001
+        |Этап: STAGE1
+        |Статус: SUCCESS
+        |Время: 2021-04-21T10:10:10Z""".stripMargin)
+  }
+
+}
